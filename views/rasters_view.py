2dd3d9d96c51bc04dbd071798ba4556e363d6c76
Sequestree <sequestree008@gmail.com>
2026-04-09 17:44:50 +0800

    Fix formatting in rasters_view.py

--- views/rasters_view.py
+++ views/rasters_view.py
@@ -23,7 +23,7 @@ def run(rasters, caption, range_min, range_max):
     print(f"TiTiler endpoint: {config.TITILER_SERVER}")
 
     m.split_map(
-        left_layer=biomass_rasters[left_year],
+       left_layer=biomass_rasters[left_year],
         right_layer=biomass_rasters[right_year],
         left_args={'palette': 'Greens', 
                    'vmin': range_min, 
